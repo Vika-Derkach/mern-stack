43dd0e389de7140f704004ba390fb76578d11675
Viktoriia Derkach <viktoriia.derkach@plus4u.net>
2021-06-27 19:03:24 +0300

    creating register: connect model, check if there is a user, create a new

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,26 @@
 const {Router} = require('express');
+const bcypt = require('bcrypt')
+const User = require('../models/User')
 const router = Router()
 // /api/auth + /register
 router.post('/register', async (req, res, next) => {
  try {
    const {email, password} = req.body
-   
+   const candidate = await User.findOne({email})
+///check if there are no the same user
+   if (candidate) {
+     return  res.status(400).json({ message: "Such user already exists"})
+   }
+   ///якщо такого немає то створюємо нового User
+   const hashedPassword = await bcrypt.hash(password, 11)//шифруємо пароль
+   const user = new User({email, password: hashedPassword})
+
+///чекаємо  поки він збережеться
+   await user.save()
+//передаємо на фронт
+   res.status(201).json({message: 'User has been created'})
+
+
  }  catch (error) {
     next(error);
   }
