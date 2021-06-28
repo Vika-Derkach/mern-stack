29cb003ac0d4d251e1f0af478b8dc116d0e70ee2
Viktoriia Derkach <viktoriia.derkach@plus4u.net>
2021-06-28 17:42:25 +0300

    routes in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "materialize-css";
+import { BrowserRouter as Router } from "react-router-dom";
+import useRoutes from "./routes";
 function App() {
+  const routes = useRoutes(false);
   return (
-    <div className="container">
-      <h1>Hello world!!</h1>
-    </div>
+    <Router>
+      <div className="container">{routes}</div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/CreatePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+const CreatePage = () => {
+  return (
+    <div>
+      <h1>Create Page</h1>
+    </div>
+  );
+};
+export default CreatePage;
